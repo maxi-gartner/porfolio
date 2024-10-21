e041882bd75be0c126c36d41861965cf0912ff1e
maxi-gartner <gartner.maxi@hotmail.com>
2024-10-21 00:45:56 -0300

    data de proyecto

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,7 +14,7 @@ const config: Config = {
       },
       backgroundImage: {
         "gradient-cover":
-          "linear-gradient(90.21deg, rgba(70, 54, 124, 0.5) -5.91%, rgba(70, 47, 189, 0.5) 111.58%)",
+          "linear-gradient(90.21deg, rgba(170, 54, 124, 0.5) -5.91%, rgba(70, 47, 189, 0.5) 111.58%)",
       },
     },
   },
