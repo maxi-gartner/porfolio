9a0dcec5d7702178a6a5ac60d00e7b8a16f76d94
maxi-gartner <gartner.maxi@hotmail.com>
2024-10-21 00:32:55 -0300

    instalacion de partiles

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,12 @@
+import CoverParticles from "@/components/cover-particles";
+
 export default function Home() {
-  return <h1>Page home</h1>;
+  return (
+    <main>
+      <div className="flex min-h-[100vh] h-full bg-no-repeat bg-gradient-cover">
+        <CoverParticles />
+        <p>introduccion</p>
+      </div>
+    </main>
+  );
 }
