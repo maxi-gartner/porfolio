bfa495a77d5d5e8fca8259816d3b2ffc501ac40b
maxi-gartner <gartner.maxi@hotmail.com>
2024-10-28 02:53:45 -0300

    pagina de proyectos terminada

--- components/projects.tsx
+++ components/projects.tsx
@@ -1,33 +1,31 @@
 import Link from "next/link";
-import Image from "next/image"; // Asegúrate de importar el componente Image
+import Image from "next/image";
 
 const projects = [
   {
-    name: "Bug Fix",
     description: "E-comerce Informatico",
+    logo: "/bug_fix_logo.png",
+    secondLogo: "/bugFixTitle2.png",
     url: "https://bug-1wmh25i6o-maxigartners-projects.vercel.app/",
-    imgSrc: "/bug-fix.png", // Asegúrate de la ruta correcta
-    scrollbarClass: "scrollbar-bugfix", // Clase para scrollbar
+    imgSrc: "/bug-fix.png",
   },
   {
-    name: "Home Essentials",
-    description: "E-comerce Venta de articulos de hogar",
+    description: "E-comerce Articulos de Hogar",
+    logo: "/HELogo.png",
     url: "https://home-essentials-front-dev-propia-erzjk9ssi.vercel.app/",
     imgSrc: "/homeEssentials.png",
-    scrollbarClass: "scrollbar-home-essentials", // Clase para scrollbar
   },
   {
-    name: "Gestor De Eventos",
-    description: "Gestion y organizacion de eventos",
+    description: "Gestion y Organizacion de eventos",
+    logo: "/gestorDeEventosFilter.png",
     url: "https://gestor-de-eventos-front.vercel.app/",
     imgSrc: "/gestorDeEventos.png",
-    scrollbarClass: "scrollbar-gestor-eventos", // Clase para scrollbar
   },
 ];
 
 export default function WebApp() {
   return (
-    <div className="h-full mt-10 w-screen">
+    <div className="h-full mt-10 w-screen pb-16">
       <div className="flex justify-evenly relative flex-wrap">
         {projects.map((project, index) => (
           <Link
@@ -35,29 +33,39 @@ export default function WebApp() {
             key={index}
             className="flex flex-col pt-5 mx-2 w-96"
           >
-            <div className="w-full max-w-sm rounded-xl min-h-16 sm:min-h-auto h-[8rem] relative transition-transform duration-300">
-              <h1 className="text-black text-3xl p-5 text-center">
-                {project.name}
-              </h1>
-              <h1 className="text-black text-3xl text-center">
+            <div className="w-full max-w-sm rounded-xl relative transition-transform duration-300">
+              <div className="flex items-center justify-center md:p-5">
+                <Image
+                  src={project.logo}
+                  className="w-auto h-16 mr-2 rounded-xl"
+                  alt={`Img Logo`}
+                  width={500}
+                  height={500}
+                />
+                {project.secondLogo && (
+                  <Image
+                    src={project.secondLogo}
+                    className="w-auto h-16 rounded-xl"
+                    alt={`Img Logo`}
+                    width={500}
+                    height={500}
+                  />
+                )}
+              </div>
+              <h2 className="text-xl py-1 md:py-0 text-center text-secondary mb-2 md:font-semibold">
                 {project.description}
-              </h1>
+              </h2>
             </div>
             <div
-              className={`w-full max-w-sm bg-white rounded-xl min-h-96 sm:min-h-auto h-[35rem] border-4 border-black relative mb-10 shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)] transition-transform duration-300 overflow-y-auto scrollbar ${project.scrollbarClass}`}
+              className={`w-full max-w-sm rounded-xl min-h-96 sm:min-h-auto h-[35rem] border-4 border-black relative mb-10 shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)] transition-transform duration-300 overflow-y-auto scrollbar scrollbar-secondary bg-black group`}
             >
               <Image
                 src={project.imgSrc}
                 className="w-full h-auto object-cover rounded-xl"
-                alt={project.name}
+                alt="Project Image"
                 width={500}
-                height={500} // Cambia la altura según sea necesario
+                height={500}
               />
-              <div className="w-full h-full bg-black absolute top-0 left-0 flex justify-center items-center bg-opacity-40 hover:bg-opacity-0">
-                <div className="flex flex-col justify-center items-center p-10 pb-16">
-                  <h2 className="text-white text-xl">{project.description}</h2>
-                </div>
-              </div>
             </div>
           </Link>
         ))}
